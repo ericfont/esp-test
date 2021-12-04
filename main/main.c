863a6c1de53d3ae8bc328750dffe778eca3694ab
Eric Fontaine <ericfontainejazz@gmail.com>
2021-12-04 03:26:11 -0500

    failed attempt at manually adding each source

--- main/main.c
+++ main/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "esp_system.h"
-#include "aoo/aoo.h"
+#include "aoo/include/aoo/aoo.h"
 
 void app_main(void)
 {
-    printf("aoo version: %d.%d.%d\n", AOO_VERSION_MAJOR, AOO_VERSION_MINOR, AOO_VERSION_BUGFIX);
+//    printf("aoo version: %d.%d.%d\n", AOO_VERSION_MAJOR, AOO_VERSION_MINOR, AOO_VERSION_BUGFIX);
+
+	aoo_initialize();
+    printf("default source buffer size: %f\n", AOO_SOURCE_BUFFER_SIZE);
 }

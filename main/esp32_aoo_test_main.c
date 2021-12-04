7c673f69d8269a63ff7b9aebdd24f137f9a4479c
Eric Fontaine <ericfontainejazz@gmail.com>
2021-12-04 10:30:10 -0500

    build works & runs, configure with:
    idf.py -DAOO_CODEC_OPUS=OFF -DOPUS_INSTALL_PKG_CONFIG_MODULE=OFF -DOPUS_INSTALL_CMAKE_CONFIG_MODULE=OFF -DAOO_BUILD_PD_EXTERNAL=OFF -DAOO_BUILD_SC_EXTENSION=OFF -DAOO_BUILD_SHARED_LIBRARY=OFF -DAOO_NET=OFF set-target esp32

--- main/esp32_aoo_test_main.c
+++ main/esp32_aoo_test_main.c
@@ -4,8 +4,8 @@
 
 void app_main(void)
 {
-//    printf("aoo version: %d.%d.%d\n", AOO_VERSION_MAJOR, AOO_VERSION_MINOR, AOO_VERSION_BUGFIX);
+    printf("aoo version: %d.%d.%d\n", AOO_VERSION_MAJOR, AOO_VERSION_MINOR, AOO_VERSION_BUGFIX);
 
-	aoo_initialize();
+    aoo_initialize();
     printf("default source buffer size: %f\n", AOO_SOURCE_BUFFER_SIZE);
 }
